8e15777ff62e7c7b19e4f0c8c78e16383de01388
ThaissaDomiciano <thaissadomiciano@icloud.com>
2025-09-02 16:39:52 -0300

    styles/atualizando estilos da tela de login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen grid place-items-center bg-muted/20">
+  return <div className="min-h-screen grid place-items-center bg-gradient-to-br from-[#065F46] to-[#057C58]">
             {children}
         </div>;
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,9 +6,15 @@ import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Card, 
+         CardHeader, 
+         CardTitle, 
+         CardDescription 
+        } from '@/components/ui/card';
 
 const schema = z.object({
   matricula: z.string().min(1, 'Informe a matrícula'),
@@ -20,10 +26,8 @@ export default function LoginPage() {
   const router = useRouter();
   const search = useSearchParams();
   const nextUrl = search.get('from') || '/home';
-
   const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
-
   const { register, handleSubmit, formState: { errors } } =
     useForm<FormData>({ resolver: zodResolver(schema) });
 
@@ -50,32 +54,44 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="w-full max-w-sm bg-white p-6 rounded-2xl shadow">
-      <h1 className="text-xl font-semibold mb-4">Entrar</h1>
+    <main className="fixed inset-0 grid grid-cols-1 md:grid-cols-2">
+      <section className="hidden md:flex items-center justify-center p-10">
+        <Image src="/imgLogin.svg" alt="imagem do login" width={400} height={400} />
+      </section>
 
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <div>
-          <Label htmlFor="matricula">Matrícula</Label>
-          <Input id="matricula" {...register('matricula')} placeholder="Ex.: 123456" />
-          {errors.matricula && (
-            <p className="text-sm text-red-600 mt-1">{errors.matricula.message}</p>
-          )}
-        </div>
+      <section className="md:col-start-2 flex items-stretch">
+        <Card className="w-full h-full border-none rounded-none md:rounded-tl-[40%] bg-[var(--background)] shadow overflow-hidden flex">
+          <div className="m-auto w-full max-w-sm">
+            <CardHeader className="space-y-2 p-0">
+              <p className='flex items-center gap-2 text-[var(--verde-800)] font-bold text-xl'><Image src="/logoWeb.svg" alt="Logo" width={60} height={60} />Gestão de Benefícios</p>
+              <CardTitle className="text-4xl font-semibold">Bem-vindo de volta</CardTitle>
+              <CardDescription className="text-3xl font-semibold">Realize seu login</CardDescription>
+            </CardHeader>
 
-        <div>
-          <Label htmlFor="senha">Senha</Label>
-          <Input id="senha" type="password" {...register('senha')} />
-          {errors.senha && (
-            <p className="text-sm text-red-600 mt-1">{errors.senha.message}</p>
-          )}
-        </div>
+            <form onSubmit={handleSubmit(onSubmit)} className="mt-6 space-y-4">
+              <div>
+                <Label htmlFor="matricula" className='mb-2'>Matrícula</Label>
+                <Input id="matricula" {...register('matricula')} placeholder="Digite sua matrícula" 
+                className='bg-[var(--cinza-200)]' />
+                {errors.matricula && <p className="text-sm text-[var(--error)] mt-1">{errors.matricula.message}</p>}
+              </div>
 
-        {errorMsg && <p className="text-sm text-red-600">{errorMsg}</p>}
+              <div>
+                <Label htmlFor="senha" className='mb-2'>Senha</Label>
+                <Input id="senha" type="password" {...register('senha')} placeholder="Digite sua senha"
+                className='bg-[var(--cinza-200)]' />
+                {errors.senha && <p className="text-sm text-[var(--error)] mt-1">{errors.senha.message}</p>}
+              </div>
 
-        <Button type="submit" className="w-full" disabled={loading}>
-          {loading ? 'Entrando...' : 'Entrar'}
-        </Button>
-      </form>
-    </div>
+              {errorMsg && <p className="text-sm text-[var(--error)]">{errorMsg}</p>}
+
+              <Button type="submit" className="w-full bg-[var(--verde-800)] text-[var(--branco)]" disabled={loading}>
+                {loading ? 'Entrando...' : 'Entrar'}
+              </Button> 
+            </form>
+          </div>
+        </Card>
+      </section>
+    </main>
   );
 }
